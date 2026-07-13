9eb0eacda6fecf6873f3fbdd498c4fe73664df1c
TSUMYCH <120321842+Pantapan1@users.noreply.github.com>
2026-07-13 21:42:32 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Запуск бота из корня проекта. Можешь вызвать как:
+  python main.py
+  python -m bot.main
+"""
+if __name__ == '__main__':
+    from bot.main import main
+    main()
